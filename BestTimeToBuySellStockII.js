34c002e7cbea921c00b319fa7dbae4421ec77274
Sushant Chhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-07-19 21:37:00 -0500

    Create BestTimeToBuySellStockII.js

---
+++ BestTimeToBuySellStockII.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let totalProfit = 0;
+
+    for(let i = 0; i < prices.length ; i++){        
+        if(prices[i] > prices[i-1]){
+            totalProfit += prices[i] - prices[i-1];
+        }
+    }
+
+    return totalProfit;
+};
